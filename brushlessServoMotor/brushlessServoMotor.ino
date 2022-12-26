84b417786e07de02864ab0d5bb1bd95e4717b8ed
dbsalazarr <dbsalazarr@gmail.com>
2022-12-26 18:14:50 -0500

    Control del Ventilador Potenciometro

--- brushlessServoMotor/brushlessServoMotor.ino
+++ brushlessServoMotor/brushlessServoMotor.ino
@@ -7,52 +7,95 @@
 
 #include <Servo.h> // librería para controlar servomotores
 
-// Pulsadores
-int pinPulsador= 7; 
-int 
-// Pin del ventilador
-int ventilador = 9; 
+// PULSADORES
+int pulsadorInterruptor = 2; 
+int pulsadorGiro = 3;
+int potenVelocidadVentilador = A0;
+
+// PIN PARA EL VENTILADOR
+int ventilador = 9;  
 
 
-// CONTROL DEL VENTILADOR 
-int girar = 0;
+// VARIABLES DE CONTROL
+int interruptor = 0;
+int giroVentilador = 0;
+int velocidadVentilador = 0;
+
 int posicionVentilador = 0;
+int retrasoServo = 15;
 
-
-Servo servo; // declaración de un objeto servo
+// VARIABLE - OBJETO SERVO PARA EL VENTILADOR
+Servo servoVentilador; 
 void setup() {
-  servo.attach(ventilador); // conectamos el servo al pin 9
-  pinMode(pinPulsador, INPUT); // configuramos el pin del botón como entrada
-  pinMode(ventilador, OUTPUT);
+	
+	// ENTRADAS
+	pinMode(pulsadorInterruptor, INPUT); 
+	pinMode(pulsadorGiro, INPUT);
+	pinMode(potenVelocidadVentilador, INPUT);
+
+	// SALIDAS
+	pinMode(ventilador, OUTPUT);
+
+	// CONFIGURANDO EL PUERTO ARDUINO AL VENTILADOR
+	servoVentilador.attach(ventilador);
+	// INICIANDO EL VENTILADOR EN LA POSICIÓN PREDETERMINADA
+	servoVentilador.write( posicionVentilador);
+
 }
 
 void loop() {
 
-  digitalWrite(ventilador, HIGH);
+	// CONFIGURACIÓN DE LAS VARIABLES DE CONTROL
 
-	if( digitalRead(pinPulsador)){
-			girar += 1;
+	if( digitalRead(pulsadorInterruptor) == 1){
+		interruptor += 1;
+		delay(500);
 	}
 
-	if( girar == 1){
-		for (int i = 0; i <= 180; i += 1) { // giro desde 0 hasta 180 grados
-      servo.write(i); // movemos el servo a la posición i
-			posicionVentilador = i;
-      delay(15); // esperamos 15 milisegundos
+	if( digitalRead(pulsadorGiro) == 1){
+		giroVentilador += 1;
+		delay(500);
+	}
 
-    }
-    for (int i = 180; i >= 0; i -= 1) { // giro desde 180 hasta 0 grados
-      servo.write(i);
-			posicionVentilador = i;
-			delay(15);
+	/* 
+		CODIGO DE CONTROL PARA EL FUNCIONAMIENTO
+		- Encendido - Apagado
+		- Velocidad
+	*/	
+	
+	// VENTILADOR ENCENDIDO
+	if (interruptor == 1){
+		// CONTROL DE VELOCIDAD	
+		velocidadVentilador = map(potenVelocidadVentilador, 0, 1023, 0, 255);
+		analogWrite(ventilador, velocidadVentilador);
+
+		switch(giroVentilador){
+			case 0:
+				// GIRO DE 0º - 180º
+				for (posicionVentilador = 0; posicionVentilador <= 180; posicionVentilador += 1) {
+					servoVentilador.write(posicionVentilador); // 
+					delay(retrasoServo);
+
+				}
+				// GIRO DE 180º - 0º
+				for (posicionVentilador = 180; posicionVentilador >= 0; posicionVentilador -= 1) { // giro desde 180 hasta 0 grados
+					servoVentilador.write(posicionVentilador);
+					delay(retrasoServo);
+				}
+			break;
+			case 1:
+				// MANTENER LA POSICIÓN
+				servoVentilador.write(posicionVentilador);
+			break;
+			default :
+				giroVentilador = 0;
+			break;
 		}
-	}
 
-	if( girar == 2 ){
-		servo.write(posicionVentilador);
 	}
-
-	if( girar == 3){
-			girar = 1;
+	// VENTILADOR APAGADO
+	if( interruptor == 2){
+		analogWrite(ventilador, 0);
+		interruptor = 0;
 	}
 }
