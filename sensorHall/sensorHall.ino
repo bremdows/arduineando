7a70442df6834cde1b5404ffeea6ca633a735882
Bremdow D. Salazar Roa <bremdows@gmail.com>
2023-01-26 18:06:17 -0500

    Sensor Hall - UGN3503

--- sensorHall/sensorHall.ino
+++ sensorHall/sensorHall.ino
@@ -1,15 +1,22 @@
-// PIN ANALOGICO PARA EL SENSOR HALL
-int sensorHall = A0;
+// VARIABLES DE ENTRADA
+int pinAnalogOut = A0;
+int flujoMagneticoLeido = 0;
+int flujoMagneticoMedido = 0;
 
 
 
 void setup() {
-  // INICIANDO EL MONITOR SERIAL
-  Serial.begin(38400);
-  Serial.println("SENSOR LISTO");
+  // Iniciando el Monitor Serial
+  Serial.begin(9600);
+  Serial.println("SENSOR LISTO PARA INICIALIZARLA");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(sensorHall));
+  flujoMagneticoLeido = analogRead(pinAnalogOut);
+  flujoMagneticoMedido = 3.7*flujoMagneticoLeido - 1937;
+  Serial.print("Campo Magnético:" );
+  Serial.print(flujoMagneticoMedido);
+  Serial.print(" G \n");
+  delay(1000);
 }
