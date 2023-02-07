1dcbf28c15a5100db1beedde3c2288aa8a04a9d6
bremdows <bremdows@gmail.com>
2023-02-06 22:07:09 -0500

    Calibración del sensor

--- sensorHall/sensorHall.ino
+++ sensorHall/sensorHall.ino
@@ -1,24 +1,66 @@
+#include <LiquidCrystal.h>    // importa libreria
 // VARIABLES DE ENTRADA
 int pinAnalogOut = A0;
 int flujoMagneticoLeido = 0;
 int flujoMagneticoMedido = 0;
+int pinPulsadorCalibrador = 8;
 
+// INICIALIZANDO EL LCD
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);  // pines RS, E, D4, D5, D6, D7 de modulo 1602A
 
+// VARIABLES DE MEDICIÓN
+int valorCalibrado = 0;
+
+int calibrarInstrumento( ){
+  for(int i = 0; i < 10; i++){
+    lcd.setCursor(0, 0);
+    lcd.print("Calibrando el");
+    lcd.setCursor(0, 1);
+    lcd.print("dispositivo ...");
+    
+
+    valorCalibrado += analogRead(pinAnalogOut);
+    delay(500);
+  }
+  valorCalibrado /= 10;
+  return valorCalibrado;
+}
 
 void setup() {
+  // lcd.begin(16, 2);     // inicializa a display de 16 columnas y 2 lineas
   // Iniciando el Monitor Serial
   Serial.begin(9600);
-  Serial.println("SENSOR LISTO PARA INICIALIZARLA");
+
+  // INICIALIZANDO EL PULSADOR
+  pinMode(pinPulsadorCalibrador, INPUT);
+  
 }
 
 void loop() {
-  
+  /*  CALIBRACIÓN DEL INSTRUMENTO  */
+  lcd.begin(16, 2);
   flujoMagneticoLeido = analogRead(pinAnalogOut);
   Serial.println(flujoMagneticoLeido);
-  //flujoMagneticoMedido = 3.7*flujoMagneticoLeido - 2553;
-  
-  Serial.print("Campo Magnético:" );
-  Serial.print(flujoMagneticoMedido);
+
+  /* CALIBRANDO EL DISPOSITIVO */
+  if( digitalRead(pinPulsadorCalibrador) == HIGH)  {
+    calibrarInstrumento();
+    delay(200);
+  }
+
+
+  /* FIN DE LA CALIBRACIÓN */
+
+  lcd.setCursor(0, 0);      // ubica cursor en columna 0, linea 0
+  lcd.print("Campo Magnetico ");  // escribe el texto en pantalla
+  lcd.setCursor(0, 1);      // ubica cursor en columna 0, linea 1
+  flujoMagneticoLeido = analogRead(pinAnalogOut);
+  flujoMagneticoMedido = 3.75*(flujoMagneticoLeido - valorCalibrado);
   Serial.print(" G \n");
+  lcd.print(flujoMagneticoMedido);   // escribe valor en segundos devuelto por funcion millis()
+  lcd.print(" Gauss.");  // imprime a continuacion segundos
   delay(1000);
+
 }
+
+
